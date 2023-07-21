bf11d0d6f0f0aa3ac5f5c54e9393c14e0e3d8b7e
Gil Hanan <gil.hanan@gmail.com>
2023-07-22 00:08:32 +0300

    toggleRTLGlobal tests validates setRTLEnabledValue

--- src/content/rtl-utils.test.ts
+++ src/content/rtl-utils.test.ts
@@ -1,6 +1,5 @@
 import { applyRTLToMutations, toggleRTLGlobal } from "./rtl-utils";
-
-jest.mock("../shared/rtl-enabled-storage");
+import * as rtlEnabledStorage from "../shared/rtl-enabled-storage";
 
 const RTL_CLASS = "chat-gpt-rtl";
 const RTL_ENABLED_CLASS = "chat-gpt-rtl-enabled";
@@ -102,8 +101,13 @@ describe("applyRTLToMutations", () => {
 });
 
 describe("toggleRTLGlobal", () => {
+  let setRTLEnabledValueMock: jest.SpyInstance;
+
   beforeEach(() => {
     document.body.className = "";
+    setRTLEnabledValueMock = jest
+      .spyOn(rtlEnabledStorage, "setRTLEnabledValue")
+      .mockImplementation(jest.fn());
   });
 
   it.each([
@@ -116,6 +120,7 @@ describe("toggleRTLGlobal", () => {
       expect(document.body.classList.contains(RTL_ENABLED_CLASS)).toBe(
         expected,
       );
+      expect(setRTLEnabledValueMock).toHaveBeenCalledWith(enabled);
     },
   );
 
@@ -131,6 +136,7 @@ describe("toggleRTLGlobal", () => {
       expect(document.body.classList.contains(RTL_ENABLED_CLASS)).toBe(
         expected,
       );
+      expect(setRTLEnabledValueMock).toHaveBeenCalledWith(enabled);
     },
   );
 });
