2b5984cb0b9add79a4a3e443030c830ffa3cc213
Gil Hanan <gil.hanan@gmail.com>
2023-09-22 03:52:36 +0300

    Unit tests coverage 100%

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,js}"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFiles: ["./jest.setup.js"],
